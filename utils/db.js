7850bd3d16cfabef759421cbb7761cb3eb74b419
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-08-07 18:47:26 -0700

    added db.js

--- utils/db.js
+++ utils/db.js
@@ -1,33 +1,41 @@
-import mongoose from 'mongoose';
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    const mongoURI = `mongodb://${host}:${port}/${database}`;
-    mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-
-    this.connection = mongoose.connection;
-
-    this.connection.on('error', (err) => {
-      console.error('MongoDB connection error:', err);
-    });
-
-    this.connection.once('open', () => {
-      console.log('MongoDB connected successfully');
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
     });
   }
 
   isAlive() {
-    return this.connection.readyState === 1;
+    return Boolean(this.db);
+  }
+
+  async nbUsers() {
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
+  }
+
+  async nbFiles() {
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
   }
 }
 
 const dbClient = new DBClient();
+
 export default dbClient;
