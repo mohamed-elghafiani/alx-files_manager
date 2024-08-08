c2fe8141bdf8cb8915b7a60c2973938508a4ebd0
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-08-07 18:54:16 -0700

    added First API

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import authRoutes from './routes/authRoutes';
+import controllerRouting from './routes/index';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-mongoose.connect('mongodb://localhost:27017/files_manager', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+controllerRouting(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
